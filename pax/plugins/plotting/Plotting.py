9e8e17a52dd38870ae846af724b80372b76fa3c4
Tianyu Zhu <tz2263@columbia.edu>
2018-02-19 12:59:17 -0600

    another syntax

--- pax/plugins/plotting/Plotting.py
+++ pax/plugins/plotting/Plotting.py
@@ -738,11 +738,9 @@ class PeakViewer(PlotBase):
         event_text += 'Event recorded at %s UTC, %09d ns\n' % (
             epoch_to_human_time(self.trigger_time_ns),
             self.trigger_time_ns % units.s)
-        '''
-        suspicious_channels = np.where(event.is_channel_suspicious)[0]
-        event_text += 'Suspicious channels (# hits rejected):\n ' + ', '.join([
-            '%s (%s)' % (ch, event.n_hits_rejected[ch]) for ch in suspicious_channels]) + '\n'
-        '''
+        # suspicious_channels = np.where(event.is_channel_suspicious)[0]
+        # event_text += 'Suspicious channels (# hits rejected):\n ' + ', '.join([
+        #     '%s (%s)' % (ch, event.n_hits_rejected[ch]) for ch in suspicious_channels]) + '\n'
         self.fig.text(x, y, self.wrap_multiline(event_text, self.max_characters), verticalalignment='top')
         self.peak_text = self.fig.text(x, start_y + 3 * row_y + y_sep_middle, '', verticalalignment='top')
 
